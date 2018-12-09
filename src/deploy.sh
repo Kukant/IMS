c6c99074bc9b01e7fe59fa6e2ef31ab0753f9e05
Tomas Kukan <kukant96@gmail.com>
2018-12-09 16:37:17 +0100

    deploy python script

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cp ../doc/dokumentace.pdf .
-zip 04_xkukan00_xknetl.zip main.cpp main.h Makefile dokumentace.pdf
+zip 04_xkukan00_xknetl.zip main.cpp main.h Makefile dokumentace.pdf get_statistics.py
 rm dokumentace.pdf
